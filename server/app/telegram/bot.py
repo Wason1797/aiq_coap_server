fb848a236524cc23a93e1dbd9d86e6e7cfacc80a
Wladymir Brborich <wabrborich@hotmail.com>
2024-01-28 11:25:05 +0100

    Improvements to the telegram bot manager
    * Fix types
    * Create an initialization function
    * Don't use the env manager directly
    * Wrap the send notification function locally

--- server/app/telegram/bot.py
+++ server/app/telegram/bot.py
@@ -1,26 +1,31 @@
-from typing import Any, Coroutine
+from typing import Awaitable, Callable, Iterable, Optional
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
 from aiogram.filters import CommandStart, Command
 from aiogram.types import Message
 
-from app.config.env_manager import get_settings
-
-EnvManager = get_settings()
-ALLOW_LIST = tuple(EnvManager.get_allowed_users())
-
 
 class ManagementBot:
-    _bot = Bot(EnvManager.BOT_TOKEN, parse_mode=ParseMode.HTML)
+    _bot: Optional[Bot] = None
+    _allow_list: Optional[tuple] = None
+    _notification_user: Optional[int] = None
     _dispatcher = Dispatcher()
-    _commands: dict[str, Coroutine[str, Any, None]] = dict()
+    _commands: dict[str, Callable[[], Awaitable[str]]] = dict()
+
+    @classmethod
+    def init_bot(cls, token: str, allow_list: Iterable, admin_user: int):
+        cls._bot = Bot(token, parse_mode=ParseMode.HTML)
+        cls._allow_list = tuple(allow_list)
+        cls._notification_user = admin_user
 
     @classmethod
     async def start_polling(cls) -> None:
-        return await cls._dispatcher.start_polling(cls._bot)
+        return await cls._dispatcher.start_polling(cls.bot())
 
     @classmethod
     def bot(cls) -> Bot:
+        if not cls._bot:
+            raise Exception("Bot not initialized")
         return cls._bot
 
     @classmethod
@@ -28,12 +33,23 @@ class ManagementBot:
         return key in cls._commands
 
     @classmethod
-    def get_command(cls, key: str) -> Coroutine[str, Any, None]:
+    def get_command(cls, key: str) -> Callable[[], Awaitable[str]]:
         return cls._commands[key]
 
     @classmethod
-    def register_commad(cls, key: str, callback: Coroutine[str, Any, None]) -> None:
+    def register_commad(cls, key: str, callback: Callable[[], Awaitable[str]]) -> None:
         cls._commands[key] = callback
+
+    @classmethod
+    def get_allow_list(cls) -> tuple:
+        return cls._allow_list if cls._allow_list is not None else tuple()
+
+    @classmethod
+    async def send_notification(cls, text: str) -> None:
+        if cls._notification_user is None:
+            print("Admin user not found, cannot send notification")
+            return
+        await cls.bot().send_message(cls._notification_user, text)
 
 
 @ManagementBot._dispatcher.message(CommandStart())
@@ -47,7 +63,7 @@ async def command_summary_handler(message: Message):
     if not ManagementBot.has_command("summary"):
         await message.answer("Command not supported")
 
-    if message.from_user and message.from_user.id not in ALLOW_LIST:
+    if message.from_user and message.from_user.id not in ManagementBot.get_allow_list():
         await message.answer("User not allowed")
         return
 
