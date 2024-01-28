413c05bddb00a96d1318d088a89c0c30cebddaec
Wladymir Brborich <wabrborich@hotmail.com>
2024-01-28 11:26:01 +0100

    Improvements to AiqDataResource
    * Don't use the environment manager directly
    * Use the new utility function to send notifications in the management bot

--- server/app/resources/aiq_data.py
+++ server/app/resources/aiq_data.py
@@ -1,27 +1,27 @@
 import traceback
+from typing import Callable
 
 import aiocoap.resource as resource  # type: ignore
 from aiocoap import CHANGED, INTERNAL_SERVER_ERROR, Message  # type: ignore
+from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.config.env_manager import get_settings
-from app.repositories.postgres.database import PostgresqlConnector
 from app.repositories.postgres.managers import AiqDataManager
 from app.telegram.bot import ManagementBot
 
-EnvManager = get_settings()
-
 
 class AiqDataResource(resource.Resource):
-    def __init__(self):
+    def __init__(self, location_id: str, session_manager: Callable[[], AsyncSession]):
         super().__init__()
+        self.location_id = location_id
+        self.session_manager = session_manager
 
     async def render_put(self, request) -> Message:
         try:
             message = request.payload.decode("ascii")
-            session = PostgresqlConnector.get_session()
-            await AiqDataManager.save_sensor_data(session, message, EnvManager.LOCATION_ID)
+            session = self.session_manager()
+            await AiqDataManager.save_sensor_data(session, message, self.location_id)
         except Exception:
             trace = traceback.format_exc()
-            await ManagementBot.bot().send_message(EnvManager.get_notification_user(), f"An error occurred:\n {trace}")
+            await ManagementBot.send_notification(f"An error occurred:\n {trace}")
             return Message(code=INTERNAL_SERVER_ERROR, payload="")
         return Message(code=CHANGED, payload="")
