2c1a07dcdb862b8b2f4c2f30a3eccc5ddb648a35
Wladymir Brborich <wabrborich@hotmail.com>
2024-01-28 12:50:12 +0100

    Include forwarding option in the main resource endpoint

--- server/app/resources/aiq_data.py
+++ server/app/resources/aiq_data.py
@@ -1,5 +1,5 @@
 import traceback
-from typing import Callable
+from typing import Callable, Optional
 
 import aiocoap.resource as resource  # type: ignore
 from aiocoap import CHANGED, INTERNAL_SERVER_ERROR, Message  # type: ignore
@@ -7,17 +7,23 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from app.repositories.postgres.managers import AiqDataManager
 from app.telegram.bot import ManagementBot
+from app.repositories.aiq_coap.managers import AiqDataCoapForwarder
+from app.repositories.aiq_coap.client import CoapClient
 
 
 class AiqDataResource(resource.Resource):
-    def __init__(self, location_id: str, session_manager: Callable[[], AsyncSession]):
+    def __init__(self, forward: bool, location_id: str, session_manager: Callable[[], AsyncSession], coap_client: Optional[CoapClient]):
         super().__init__()
+        self.forward = forward
         self.location_id = location_id
         self.session_manager = session_manager
+        self.coap_client = coap_client
 
     async def render_put(self, request) -> Message:
         try:
             message = request.payload.decode("ascii")
+            if self.forward and self.coap_client:
+                AiqDataCoapForwarder.forward_aiq_data(self.coap_client, message)
             session = self.session_manager()
             await AiqDataManager.save_sensor_data(session, message, self.location_id)
         except Exception:
