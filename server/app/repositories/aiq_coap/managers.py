cbd551cabf5caf4c16456463daf72f7bb81e2c76
Wladymir Brborich <wabrborich@hotmail.com>
2024-01-28 12:49:49 +0100

    Add a forwarder to send the data to the next server
    * This task should run in the background

---
+++ server/app/repositories/aiq_coap/managers.py
@@ -0,0 +1,22 @@
+import traceback
+from app.serializers.request import AiqDataFromStation
+from app.telegram.bot import ManagementBot
+from .client import CoapClient
+
+import asyncio
+
+
+class AiqDataCoapForwarder:
+    @staticmethod
+    def forward_aiq_data(coap_client: CoapClient, data: str) -> asyncio.Task:
+        async def forward_data_task() -> None:
+            try:
+                AiqDataFromStation.model_validate_json(data)
+                response = await coap_client.put_payload(data)
+                print(f"Forwarded {response.code}, {response.payload}")
+            except Exception:
+                trace = traceback.format_exc()
+                await ManagementBot.send_notification(f"An error occurred while forwarding:\n {trace}")
+
+        task = asyncio.create_task(forward_data_task())  # Schedule the task in the background
+        return task
