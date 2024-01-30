615efed702a9b26d3bf6333d85f448cb14b879a6
Wladymir Brborich <wabrborich@hotmail.com>
2024-01-30 11:53:47 +0100

    Add the truncate command to the telegram bot
    * This also includes some cleanups and logic to prevent all stations to answer at once

--- server/app/telegram/bot.py
+++ server/app/telegram/bot.py
@@ -1,30 +1,37 @@
+import traceback
 from typing import Awaitable, Callable, Iterable, Optional
 from aiogram import Bot, Dispatcher
 from aiogram.enums import ParseMode
-from aiogram.filters import CommandStart, Command
+from aiogram.filters import CommandStart, Command, CommandObject
 from aiogram.types import Message
+
+from app.config.env_manager import StationType
 
 
 class ManagementBot:
+    dispatcher = Dispatcher()
     _bot: Optional[Bot] = None
     _allow_list: Optional[tuple] = None
     _notification_user: Optional[int] = None
-    _dispatcher = Dispatcher()
     _commands: dict[str, Callable[[], Awaitable[str]]] = dict()
+    _station_type: Optional[StationType] = None
+    _location_id: Optional[str] = None
 
     @classmethod
-    def init_bot(cls, token: str, allow_list: Iterable, admin_user: int):
+    def init_bot(cls, token: str, allow_list: Iterable, admin_user: int, station_type: StationType, location_id: str):
         cls._bot = Bot(token, parse_mode=ParseMode.HTML)
         cls._allow_list = tuple(allow_list)
         cls._notification_user = admin_user
+        cls._station_type = station_type
+        cls._location_id = location_id
 
     @classmethod
     async def start_polling(cls) -> None:
-        return await cls._dispatcher.start_polling(cls.bot())
+        return await cls.dispatcher.start_polling(cls.bot())
 
     @classmethod
     async def stop_polling(cls) -> None:
-        return await cls._dispatcher.stop_polling()
+        return await cls.dispatcher.stop_polling()
 
     @classmethod
     def bot(cls) -> Bot:
@@ -55,22 +62,70 @@ class ManagementBot:
             return
         await cls.bot().send_message(cls._notification_user, text)
 
+    @classmethod
+    def is_requested_location(cls, requested_location: Optional[str]) -> bool:
+        if not requested_location:
+            return False
+        return cls._location_id == requested_location
 
-@ManagementBot._dispatcher.message(CommandStart())
+    @classmethod
+    def is_main_server(cls) -> bool:
+        return cls._station_type == StationType.MAIN_SERVER
+
+
+@ManagementBot.dispatcher.message(CommandStart())
 async def command_start_handler(message: Message):
+    if not ManagementBot.is_main_server():
+        return
+
     if message.from_user:
         await message.answer(f"Hello {message.from_user.id}")
 
 
-@ManagementBot._dispatcher.message(Command("summary"))
-async def command_summary_handler(message: Message):
-    if not ManagementBot.has_command("summary"):
+@ManagementBot.dispatcher.message(Command("summary"))
+async def command_summary_handler(message: Message, cmd_obj: CommandObject):
+    if not ManagementBot.is_requested_location(cmd_obj.args):
+        return
+
+    if not ManagementBot.has_command(cmd_obj.command):
         await message.answer("Command not supported")
 
     if message.from_user and message.from_user.id not in ManagementBot.get_allow_list():
         await message.answer("User not allowed")
         return
 
-    callback = ManagementBot.get_command("summary")
-    result = await callback()
+    callback = ManagementBot.get_command(cmd_obj.command)
+    try:
+        result = await callback()
+    except Exception:
+        trace = traceback.format_exc()
+        await message.answer(f"An error occurred in summary:\n {trace}")
+        return
+
+    await message.answer(result)
+
+
+@ManagementBot.dispatcher.message(Command("truncate"))
+async def command_truncate_db_handler(message: Message, cmd_obj: CommandObject):
+    if ManagementBot.is_main_server():
+        return
+
+    if not ManagementBot.is_requested_location(cmd_obj.args):
+        return
+
+    if not ManagementBot.has_command(cmd_obj.command):
+        await message.answer("Command not supported")
+
+    if message.from_user and message.from_user.id not in ManagementBot.get_allow_list():
+        await message.answer("User not allowed")
+        return
+
+    callback = ManagementBot.get_command(cmd_obj.command)
+    try:
+        result = await callback()
+    except Exception:
+        trace = traceback.format_exc()
+        await message.answer(f"An error occurred in truncate:\n {trace}")
+        return
+
     await message.answer(result)
