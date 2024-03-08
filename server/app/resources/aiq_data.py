2ba46accc7cab32bb042e3a1782a9261acfd5477
Wladymir Brborich <wabrborich@hotmail.com>
2024-03-08 16:21:47 +0100

    decode the signed payload

--- server/app/resources/aiq_data.py
+++ server/app/resources/aiq_data.py
@@ -30,19 +30,21 @@ class AiqDataResource(resource.Resource):
     async def render_put(self, request) -> Message:
         try:
             message = request.payload.decode("ascii")
+            print("[COAP] got request", message)
 
+            payload, _ = message.split("|")
             if not self.is_main_server and self.coap_client:
                 AiqDataCoapForwarder.forward_aiq_data(self.coap_client, message)
 
             try:
-                await AiqDataManager.save_sensor_data(self.main_session, message, self.location_id)
+                await AiqDataManager.save_sensor_data(self.main_session, payload, self.location_id)
             except Exception as ex:
                 ex.add_note("Could not store sensor data in main DB")
-                await AiqDataManager.save_sensor_data(self.backup_session, message, self.location_id)
+                await AiqDataManager.save_sensor_data(self.backup_session, payload, self.location_id)
                 raise ex
 
             if self.is_main_server:
-                await AiqDataManager.save_sensor_data(self.backup_session, message, self.location_id)
+                await AiqDataManager.save_sensor_data(self.backup_session, payload, self.location_id)
 
         except Exception:
             trace = traceback.format_exc()
