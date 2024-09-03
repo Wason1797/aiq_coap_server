74d68407eb6931b2d5cffeff3a8c1081a4bc4b4f
Wladymir Brborich <wabrborich@hotmail.com>
2024-09-03 16:31:43 +0200

    Add station manager

---
+++ server/app/managers/station_manager.py
@@ -0,0 +1,45 @@
+from typing import Optional
+from app.types import AsyncSessionMaker
+from sqlalchemy import select
+
+from app.repositories.db.models import Station
+
+
+class StationManager:
+    @staticmethod
+    async def register_sensor_station(session_maker: AsyncSessionMaker, name: str, station_id: Optional[int]) -> str:
+        async with session_maker() as session:
+            if station_id:
+                query = select(Station).where(Station.id == station_id).limit(1)
+                current_station = (await session.scalars(query)).first()
+
+                if current_station:
+                    current_station.name = name
+                    await session.commit()
+                    return f"Updated Sensor station {current_station.id} {current_station.name}"
+                else:
+                    return f"Sensor station with id {station_id} not found"
+            else:
+                new_station = Station(
+                    name=name,
+                )
+                session.add(new_station)
+                await session.commit()
+
+                return f"Sensor station registered with id: {new_station.id} | {new_station.name}"
+
+    @staticmethod
+    async def get_station_by_id(session_maker: AsyncSessionMaker, id: int) -> Station | None:
+        async with session_maker() as session:
+            query = select(Station).where(Station.id == id).limit(1)
+            return (await session.scalars(query)).first()
+
+    @staticmethod
+    async def get_station_summary(session_maker: AsyncSessionMaker) -> str:
+        async with session_maker() as session:
+            stations = (await session.scalars(select(Station))).all()
+
+        if not stations:
+            return "No sensor stations were found"
+        summary = "\n".join(f"{st.id}\t{st.name}" for st in stations)
+        return f"id\tname\n{summary}"
