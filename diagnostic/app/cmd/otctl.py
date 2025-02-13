cc9620fd964aabc79c32670a62e57fc55cf027b7
Wladymir Brborich <wabrborich@hotmail.com>
2025-02-13 16:09:25 +0100

    read line by line in subprocess

--- diagnostic/app/cmd/otctl.py
+++ diagnostic/app/cmd/otctl.py
@@ -4,24 +4,29 @@ import asyncio
 async def get_meshdiag_topology() -> str:
     try:
         proc = await asyncio.create_subprocess_exec(
-            "sudo ot-ctl", "meshdiag", "topology", stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+            "sudo", "ot-ctl", "meshdiag", "topology", stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
         )
 
-        stdout, stderr = await proc.communicate()
+        stdout_str = ""
 
-        stderr
+        while True:
+            line = await proc.stdout.readline()
+            if not line:
+                break
 
-        if stderr:
-            print("ERROR RUNNING COMMAND:", stderr.decode())
+            stdout_str += line.decode()
+
+        rc = await proc.wait()
+
+        if rc != 0:
+            print("ERROR RUNNING COMMAND:", await proc.stderr.readline().decode())
             return ""
 
-        output = stdout.decode()
-
-        if "Error" in output:
-            print("ERROR RUNNING COMMAND:", output)
+        if "Error" in stdout_str:
+            print("ERROR RUNNING COMMAND:", stdout_str)
             return ""
 
-        return output
+        return stdout_str
 
     except FileNotFoundError:
         print("ERROR RUNNING COMMAND: ot-ctl not installed")
